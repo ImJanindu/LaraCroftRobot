c79aef71b70f2c20000fe89bea4477c6227bb008
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-02-05 17:17:29 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -58,7 +58,7 @@ You can find my list of available commands with /help.
 """
 
 HELP_STRINGS = """
-Hey! My name is {}. I am a group management bot, here to help you get around and keep the order in your groups! I have lots of handy features, such as flood control, a warning system, a note keeping system, and even predetermined replies on certain keywords.
+Hey! My name is {}. I am a group management bot, here to help you get around and keep the order in your groups!
 
 *Main* commands available:
 ✪ /start: Starts me! You've probably already used this.
@@ -69,6 +69,7 @@ Hey! My name is {}. I am a group management bot, here to help you get around and
    ◔ in a Group: will redirect you to pm, with all that chat's settings.
 
 If you have any bugs or questions on how to use me, head to @Infinity_BOTs.
+
 {}
 """.format(
     dispatcher.bot.first_name, ""
